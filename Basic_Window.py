40013cc124c21704f1a68840533a9bb5dc3784d1
Samrat Biswas <102436136+samratpro@users.noreply.github.com>
2023-07-13 02:49:24 +0600

    Create Basic_Window.py

---
+++ Basic_Window.py
@@ -0,0 +1,15 @@
+from tkinter import *
+
+
+window = Tk()
+window.title("Automation App")  # Title
+window.geometry("900x600")   # Window Area
+
+frame = Frame(window) # Main Frame
+frame.pack()
+
+
+
+
+if __name__ == '__main__':
+    window.mainloop()
